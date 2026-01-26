17ca746663b7618096565dee84871d8c59b791fe
helton-quintans <helton.quit@gmail.com>
2026-01-26 18:04:23 -0300

    ci: add biome

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -57,7 +57,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 }
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as unknown as any),
   session: {
     strategy: "jwt",
   },
