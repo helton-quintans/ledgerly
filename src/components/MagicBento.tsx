c385bc2c341c6a24bb61e0fc20f94d1fddac2788
helton-quintans <helton.quit@gmail.com>
2026-02-07 19:36:10 -0300

    build: fix build deploy

--- src/components/MagicBento.tsx
+++ src/components/MagicBento.tsx
@@ -196,7 +196,7 @@ const ParticleCard: React.FC<{
         });
       }, index * 100);
 
-      timeoutsRef.current.push(timeoutId);
+      timeoutsRef.current.push(Number(timeoutId));
     });
   }, [initializeParticles]);
 
